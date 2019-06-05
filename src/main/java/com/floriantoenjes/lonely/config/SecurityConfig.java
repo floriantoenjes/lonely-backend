7303b2134a821af4cfad01abe4528134bb0aaa47
Florian Tönjes <skatepainter@yahoo.de>
2019-06-05 22:32:59 +0200

    Changed port and added SimpleCorsFilter

--- src/main/java/com/floriantoenjes/lonely/config/SecurityConfig.java
+++ src/main/java/com/floriantoenjes/lonely/config/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.floriantoenjes.lonely.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @Configuration
@@ -15,4 +23,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.oauth2ResourceServer()
                 .jwt();
     }
+}
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class SimpleCORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
 }
