c24d6600723e2dcd44c0631a00d8540c8498c9dd
Florian Tönjes <skatepainter@yahoo.de>
2019-06-05 18:35:10 +0200

    Changed age field to birth date

--- src/main/java/com/floriantoenjes/lonely/user/profile/Profile.java
+++ src/main/java/com/floriantoenjes/lonely/user/profile/Profile.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -23,7 +24,7 @@ public class Profile {
 
     private Sex sex;
 
-    private int age;
+    private LocalDate birthDate;
 
     private String pictureURL;
 
