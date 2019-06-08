255e7b27f11b95f95a205b3623ea42975139a387
Florian Tönjes <skatepainter@yahoo.de>
2019-06-08 17:59:19 +0200

    Setting profile in saveSettings method now

--- src/main/java/com/floriantoenjes/lonely/user/profile/ProfileRepository.java
+++ src/main/java/com/floriantoenjes/lonely/user/profile/ProfileRepository.java
@@ -2,5 +2,8 @@ package com.floriantoenjes.lonely.user.profile;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends MongoRepository<Profile, String> {
+    Optional<Profile> findByUsername(String username);
 }
--- src/main/java/com/floriantoenjes/lonely/user/settings/UserSettingsController.java
+++ src/main/java/com/floriantoenjes/lonely/user/settings/UserSettingsController.java
@@ -1,16 +1,24 @@
 package com.floriantoenjes.lonely.user.settings;
 
+import com.floriantoenjes.lonely.user.profile.Profile;
+import com.floriantoenjes.lonely.user.profile.ProfileRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/settings")
 public class UserSettingsController {
 
+    private ProfileRepository profileRepository;
+
     private UserSettingsRepository userSettingsRepository;
 
-    public UserSettingsController(UserSettingsRepository userSettingsRepository) {
+    public UserSettingsController(ProfileRepository profileRepository, UserSettingsRepository userSettingsRepository) {
+        this.profileRepository = profileRepository;
         this.userSettingsRepository = userSettingsRepository;
     }
 
@@ -21,6 +29,12 @@ public class UserSettingsController {
 
     @PostMapping
     public UserSettings saveSettings(@RequestBody UserSettings userSettings) {
+        Optional<Profile> profile = profileRepository.findByUsername(SecurityContextHolder.getContext()
+                .getAuthentication().getName());
+
+        profile.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        userSettings.setProfile(profile.get());
+
         return userSettingsRepository.save(userSettings);
     }
 
